4ccd9113dff2b724675bcb4ab1652ccee95a6732
Wajdimuh <w.elmuhtadi@gju.edu.jo>
2021-10-19 15:26:58 +0200

    start work on joystick

--- app/src/main/java/com/hbrs/MainActivity.java
+++ app/src/main/java/com/hbrs/MainActivity.java
@@ -13,9 +13,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.*;
+import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 //*************************************************************************************************
 public class MainActivity extends AppCompatActivity
@@ -23,6 +26,12 @@ public class MainActivity extends AppCompatActivity
     private static final String TAG = "MBot";
 
     private MBot mbot;
+
+    private Button up;
+    private Button down;
+    private Button left;
+    private Button center;
+    private Button right;
 
     private final static int REQUEST_BLUETOOTH_ENABLE = 1;
     private final static int REQUEST_BLUETOOTH_GET_ADDR = 2;
@@ -53,6 +62,29 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         mbot = new MBot();
+
+        JoystickView joystick = (JoystickView) findViewById(R.id.joystickView);
+        joystick.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                double anglerad = angle;
+                double v_a,v_b;
+                anglerad = ((anglerad + 180) % 360) - 180;
+                Log.i("movement",String.format("%f",anglerad));
+                v_a = strength * (45 - anglerad % 90) / 45;
+                v_b = Math.min(100.0, Math.min(2 * strength + v_a, 2 * strength - v_a));
+                if(anglerad < -90){
+                    mbot.setDrive(-1 * (int) (v_a),-1 * (int) (v_a));
+                }else if (anglerad < 0){
+                    mbot.setDrive(-1 * (int) (v_a),(int) (v_b));
+                }else if (anglerad < 90){
+                    mbot.setDrive((int) (v_b), (int) (v_a));
+                }else{
+                    mbot.setDrive((int) (v_a), -1 * (int) (v_b));
+                }
+            }
+        });
+
     }
 
     //-----------------------------------------------------------------
