b9a7561f13d653a08d72756e76775783fa16fd41
Wajdimuh <w.elmuhtadi@gju.edu.jo>
2021-10-26 18:19:30 +0200

    add red range filter

--- app/src/main/java/com/hbrs/Cameraactivity.java
+++ app/src/main/java/com/hbrs/Cameraactivity.java
@@ -7,6 +7,7 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
@@ -42,14 +43,29 @@ public class Cameraactivity extends AppCompatActivity
     {
         // Get frame
         Mat mImg = inputFrame.rgba();
+        Mat hsv = new Mat((int) mImg.size().height, (int) mImg.size().width, CvType.CV_8UC3);
 
-        //Mat mRgbaT = mImg.t();
-        //Core.flip(mImg.t(), mRgbaT, 1);
+        Imgproc.cvtColor(mImg, hsv, Imgproc.COLOR_RGB2HSV, 3);
 
-        //Imgproc.resize(mRgbaT, mRgbaT, mImg.size());
-        // return image to CameraView
+        Mat mask1 = new Mat(hsv.rows(), hsv.cols(), CvType.CV_8U, new Scalar(0));
+        Mat mask2 = new Mat(hsv.rows(), hsv.cols(), CvType.CV_8U, new Scalar(0));
+        Mat combinedmask = new Mat(hsv.rows(), hsv.cols(), CvType.CV_8U, new Scalar(0));
 
-        //mRgbaT.release();
+        Scalar lowerlowerredhue = new Scalar(0, 100, 100);
+        Scalar upperlowerredhue = new Scalar(10, 255, 255);
+
+        Scalar lowerupperredhue = new Scalar(170, 100, 100);
+        Scalar upperupperredhue = new Scalar(180, 255, 255);
+
+        Core.inRange(hsv, lowerlowerredhue, upperlowerredhue, mask1);
+        Core.inRange(hsv, lowerupperredhue, upperupperredhue, mask2);
+
+        Core.bitwise_not( mask1, mask1);
+        Core.bitwise_not( mask2, mask2);
+        Core.bitwise_and( mask1, mask2,combinedmask);
+        hsv.setTo(new Scalar(0,0,0), combinedmask);
+
+        Imgproc.cvtColor(hsv, mImg, Imgproc.COLOR_HSV2RGB, 4);
         return  mImg;
     }
 
